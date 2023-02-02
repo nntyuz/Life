590347f459a825eb8942ea18b3c8ab24eddc9e15
nnuyz <hellonedesing@gmail.com>
2023-02-02 15:23:32 +0800

    add dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'vue/multi-word-component-names': 'off'
+  }
+}
