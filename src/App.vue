53bad5212dac432820ad0b23e0e09720511383ca
nnuyz <hellonedesing@gmail.com>
2023-02-06 22:46:24 +0800

    add main files

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <HeaderComponent />
+  <section class="content">
+    <FolderComponent />
+    <router-view />
+    <TrackComponent v-if="$route.name === 'tasks'" />
+  </section>
 </template>
 
 <script>
 import HeaderComponent from './components/global/header.vue'
+import FolderComponent from './components/home/folders.vue'
+import TrackComponent from './components/home/track.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    FolderComponent,
+    TrackComponent
+  },
+  created() {
+    this.$router.push('/folder/main')
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import router from './router/router'
 import App from './App.vue'
 
-import Icon from './components/core/icon.vue'
+import OutsideDirective from './directives/outside'
 
+import Icon from './components/core/icon.vue'
+import Input from './components/core/input.vue'
+import Checkbox from './components/core/checkbox.vue'
+import Radio from './components/core/radio.vue'
+
+import './assets/styles/reset.scss'
 import './assets/styles/main.scss'
 
 const app = createApp(App)
 
-app.component('VIcon', Icon)
-
 app.use(createPinia())
+app.use(router)
 
+app.directive('outside', OutsideDirective)
+
+app
+  .component('VIcon', Icon)
+  .component('VInput', Input)
+  .component('VCheckbox', Checkbox)
+  .component('VRadio', Radio)
 app.mount('#app')
