9f304f5e6663f1642c4565dd681a7e76224d8c56
nnuyz <hellonedesing@gmail.com>
2023-08-07 20:26:01 +0300

    init

--- src/helpers/format-time.js
+++
@@ -1,6 +0,0 @@
-export default (seconds = 0) => {
-  let measuredTime = new Date(null)
-  measuredTime.setSeconds(seconds)
-  let MHSTime = measuredTime.toISOString().substr(11, 8)
-  return MHSTime
-}
--- src/helpers/save-to-localstorage.js
+++
@@ -1,8 +0,0 @@
-export default (store) => {
-  localStorage.setItem('tasks', JSON.stringify([...store.tasks]))
-  localStorage.setItem('folders', JSON.stringify([...store.folders]))
-  if (![null, undefined].includes(store.runningTask))
-    localStorage.setItem('running-task-id', JSON.stringify(store.runningTask.id))
-  if (![null, undefined].includes(store.currentTask))
-    localStorage.setItem('current-task-id', JSON.stringify(store.currentTask.id))
-}
